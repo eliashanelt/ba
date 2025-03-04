b523f1196c4b7d7c9b857f4013edfb01fad92598
Elias Hanelt <elias.hanelt@gmail.com>
2025-03-04 19:23:05 +0100

    first working portenta h7 follower

--- firmware/portentaH7/dac_test/dac_test.ino
+++ firmware/portentaH7/dac_test/dac_test.ino
@@ -1,46 +1,28 @@
-#include "AnalogOut.h"
+#include "Arduino.h"
 #include "hal/analogout_api.h"
+#include "hal/analogin_api.h"
 
-////AnalogOut *dac;
+// Our objects
 dac_t myDac;
+analogin_t myAdc;
 
 void setup() {
-  // put your setup code here, to run once:
-  const PinMap *pinMap;
-  pinMap = analogout_pinmap();
 
-  pinMode(LEDR,OUTPUT);
-  pinMode(LEDG,OUTPUT);
-  pinMode(LEDB,OUTPUT); 
+  // Convert the Arduino pin name (e.g. A0) to an Mbed pin name
+  PinName dacPin = digitalPinToPinName(A6);  // Suppose your board says A0 is a DAC
+  PinName adcPin = digitalPinToPinName(A1);  // Suppose A1 is a regular analog input
 
-  digitalWrite(LEDR, HIGH); //red LED OFF - wait for terminal: default 9600baud
-  digitalWrite(LEDG, HIGH); //HIGH is OFF!
-  digitalWrite(LEDB, HIGH);
+  // Initialize the DAC on A0
+  analogout_init(&myDac, dacPin);
 
-  // Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  Serial.println("*** DAC demo setup ***");
-  Serial.print("pin: 0x");
-  Serial.println(pinMap->pin, HEX);
-
-  ////dac = AnalogOut(pinMap->pin);
-  analogout_init_direct(&myDac, pinMap);
-  analogout_write(&myDac, 8192);
+  // Initialize the ADC on A1
+  analogin_init(&myAdc, adcPin);
 }
 
 void loop() {
-  //Serial.println("Loop | DAC demo:");
-  //digitalWrite(LEDG, HIGH);
-  //analogout_write(&myDac, 0);
-  //delay(1000);
-  //digitalWrite(LEDG, LOW);
-  //uint16_t adcValue = analogout_read_u16(A0);
-  //analogout_write(&myDac, adcValue);
-  analogout_write(&myDac, 1024);
-  //delay(1000);
-  //analogout_write(&myDac, 2048);
-  //delay(1000);
+  // Read a 16-bit value from the ADC
+  //uint16_t adcValue = analogin_read_u16(&myAdc);
+  float adcValue = analogin_read(&myAdc);
+  // Write that value to the DAC
+  analogout_write(&myDac, adcValue);
 }
